82c9d71665d2f4b341d9e101dc4cf0641a08fb28
Jaffrin Sampaul <jaffrin5404sam@gmail.com>
2023-04-26 16:24:42 +0530

    hover rotation implemented

--- src/Container/About/About.tsx
+++ src/Container/About/About.tsx
@@ -13,7 +13,9 @@ const About = () => {
         <div className="flex w-full h-[78%] justify-between items-start">
           <div className="flex w-[38%] h-full justify-center">
             <div className=" h-[60%] profile w-[85%] rounded-[30px] rotate-[-1deg]">
+              <div className="h-[100%] bg-slate-200 w-[100%] rounded-[30px] transition ease-in-out delay-150 duration-300 hover:rotate-2 rotate-[9deg]"></div>
             </div>
+
           </div>
           <div className="w-[50%] h-full bg-slate-300 ">
             <div className="flex">
