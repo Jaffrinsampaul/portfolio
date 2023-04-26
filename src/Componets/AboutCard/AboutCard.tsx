66dca5173ce71910eb7d911fdcc6c050ff4e1806
Jaffrinsampaul <jaffrin5404sam@gmail.com>
2023-04-26 20:53:55 +0530

    img change to custom image components

--- src/Componets/AboutCard/AboutCard.tsx
+++ src/Componets/AboutCard/AboutCard.tsx
@@ -1,27 +1,36 @@
 import React from "react";
 import CustomText from "../CustomText/CustomText";
 import { aboutCardType } from "../../Utills/dto";
+import CustomImage from "../CustomImage/CustomImage";
 
 type Props = {
   cardBehaviour: {
-    detail: aboutCardType,
-    style: string,
-    detailStyle: string
-  }
-}
+    detail: aboutCardType;
+    style: string;
+    detailStyle: string;
+  };
+};
 
 const AboutCard = ({ cardBehaviour }: Props) => {
   return (
     <>
       <div className={cardBehaviour.style}>
-        <img src={cardBehaviour.detail.img} alt="about" height={20} width={20}/>
+        <CustomImage
+          imageBehaviour={{
+            src: cardBehaviour.detail.img,
+            alt: "about",
+            height: 20,
+            width: 20,
+          }}
+        />
         <CustomText text={cardBehaviour.detail.title} />
-        <CustomText text={cardBehaviour.detail.subTitle}
+        <CustomText
+          text={cardBehaviour.detail.subTitle}
           style={cardBehaviour.detailStyle}
         />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default AboutCard
+export default AboutCard;
